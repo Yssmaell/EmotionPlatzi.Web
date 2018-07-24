9a77e2d38f59f5a55a8a1ffbdb462cb7555bb39c
Ysmael Alcala <rico.alcala@gmail.com>
2018-07-24 16:05:36 -0300

    Primer commit para el proyecto

---
+++ EmotionPlatzi.Web/Controllers/EmoUploaderController.cs
@@ -0,0 +1,70 @@
+﻿using EmotionPlatzi.Web.Models;
+using EmotionPlatzi.Web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmotionPlatzi.Web.Controllers
+{
+    public class EmoUploaderController : Controller
+    {
+        string serverFolderPath;
+        EmotionHelper vHelper;
+        string key;
+        EmotionPlatziWebContext db = new EmotionPlatziWebContext();
+
+        public EmoUploaderController()
+        {
+            key = ConfigurationManager.AppSettings["UPLOAD_KEY"];
+            serverFolderPath = ConfigurationManager.AppSettings["UPLOAD_DIR"];
+            vHelper = new EmotionHelper(key);            
+        }
+
+        // GET: EmoUploader
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file?.ContentLength > 0)
+                {
+                    var vPictureName = Guid.NewGuid().ToString();
+                    vPictureName += Path.GetExtension(file.FileName);
+
+                    var vRoute = Server.MapPath(serverFolderPath);
+                    vRoute = vRoute + "/" + vPictureName;
+
+                    file.SaveAs(vRoute);
+
+                    var vPicture = await vHelper.DetectAndExtracFacesAsync(file.InputStream);
+
+                    vPicture.Name = file.FileName;
+                    vPicture.Path = serverFolderPath + "/" + vPictureName;
+
+                    db.EmoPictures.Add(vPicture);
+                    await db.SaveChangesAsync();
+
+                    return RedirectToAction("Details", "EmoPictures", new { Id = vPicture.Id });
+                }                
+            }
+            catch (Exception exc)
+            { 
+
+                throw;
+            }
+            return View();
+
+        }
+
+    }
+}
---
+++ EmotionPlatzi.Web/Utils/EmotionHelper.cs
@@ -0,0 +1,132 @@
+﻿using EmotionPlatzi.Web.Models;
+using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace EmotionPlatzi.Web.Utils
+{
+    public class EmotionHelper
+    {
+
+        public EmotionServiceClient emoClient;
+        public FaceServiceClient emoClientFace;
+
+        public EmotionHelper(string vKey)
+        {
+            //emoClient = new EmotionServiceClient(vKey);
+            emoClientFace = new FaceServiceClient(vKey, "https://southcentralus.api.cognitive.microsoft.com/face/v1.0");
+        }
+
+        public async Task<EmoPicture> DetectAndExtracFacesAsync(Stream vImageString)
+        {
+            //Emotion[] vEmotion = await emoClient.RecognizeAsync(vImageString);
+
+            //var vPicture = new EmoPicture();
+
+            //vPicture.Faces = ExtractFaces(vEmotion, vPicture);
+            //return vPicture;
+
+            var emoPicture = new EmoPicture();
+            try
+            {
+                IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[] { FaceAttributeType.Emotion };
+                Face[] faces = await emoClientFace.DetectAsync(vImageString, false, false, faceAttributes);
+
+                emoPicture.Faces = ExtractFaces(faces, emoPicture); //Face
+            }
+            catch (Exception exc)
+            {
+
+                throw;
+            }
+
+            
+
+            return emoPicture;
+
+
+            }
+
+        private ObservableCollection<EmoFace> ExtractFaces(Emotion[] vEmotion, EmoPicture vPicture)
+        {
+            var lFaces = new ObservableCollection<EmoFace>();
+
+            foreach (Emotion emotion in vEmotion)
+            {
+                EmoFace vFace = new EmoFace()
+                {
+                    X = emotion.FaceRectangle.Left,
+                    Y = emotion.FaceRectangle.Top,
+                    Width = emotion.FaceRectangle.Width,
+                    Height = emotion.FaceRectangle.Height,
+                    Picture = vPicture
+                };
+                
+                vFace.Emotions = ProcessEmotions(emotion.Scores, vFace);
+                lFaces.Add(vFace);
+            }
+
+            return lFaces;
+        }
+
+        //Face
+        private ObservableCollection<EmoFace> ExtractFaces(Face[] vFaces, EmoPicture vPicture)
+        {
+            var lFaces = new ObservableCollection<EmoFace>();
+
+            foreach (var face in vFaces)
+            {
+                var vFace = new EmoFace()
+                {
+                    X = face.FaceRectangle.Left,
+                    Y = face.FaceRectangle.Top,
+                    Width = face.FaceRectangle.Width,
+                    Height = face.FaceRectangle.Height,
+                    Picture = vPicture
+
+                };
+                vFace.Emotions = ProcessEmotions(face.FaceAttributes.Emotion, vFace);
+                lFaces.Add(vFace);
+
+            }
+
+            return lFaces;
+        }
+
+        private ObservableCollection<EmoEmotion> ProcessEmotions(EmotionScores scores, EmoFace vFace)
+        {
+            ObservableCollection<EmoEmotion> lEmotion = new ObservableCollection<EmoEmotion>();
+
+            var vProperties = scores.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+            var vFilterProperties = vProperties.Where(x => x.PropertyType == typeof(float));
+
+            var vEmoType = EmoEmotionEnum.Undertermined;            
+            foreach (var prop in vFilterProperties)
+            {
+                if (!Enum.TryParse<EmoEmotionEnum>(prop.Name, out vEmoType))
+                    vEmoType = EmoEmotionEnum.Undertermined;
+
+                var vEmotion = new EmoEmotion();
+                vEmotion.Score = (float)prop.GetValue(scores);
+                vEmotion.EmotionType = vEmoType;
+                //vEmotion.EmoFaceId = vFace.Id;
+                vEmotion.Face = vFace;
+
+                lEmotion.Add(vEmotion);
+            }
+
+            return lEmotion;
+        }
+        
+
+    }
+}
